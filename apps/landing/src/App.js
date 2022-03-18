05717750fe2954407b576d90b1f16b1c1dd87c4e
Sattibabu Valle <SValle@crd.com>
2022-03-18 17:38:28 +0530

    initial cmt

---
+++ apps/landing/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Thing } from "@shared/components";
+import "./App.css";
+//added for test hhh
+function App() {
+  return (
+    <div className="App">
+      <Thing />
+    </div>
+  );
+}
+
+export default App;
